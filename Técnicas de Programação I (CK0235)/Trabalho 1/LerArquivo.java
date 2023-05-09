1fe317f886f271884e5f80bfe137bd8f8c4015db
Albert Moren <albertmorenn@gmail.com>
2023-05-09 15:02:15 -0300

    Update in trabalho
    Foi adicionado a funcionalidade de armazena os valores em ordem crescente de cpf

--- Técnicas de Programação I (CK0235)/Trabalho 1/LerArquivo.java
+++ Técnicas de Programação I (CK0235)/Trabalho 1/LerArquivo.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class LerArquivo {
     public Map<String, String> lerArquivo(String fileName){
-        Map<String , String> cpfnome = new HashMap<>();
+        Map<String , String> cpfnome = new TreeMap<>();
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader leitor = new BufferedReader(new FileReader(fileName));
             String line;
-            while((line = reader.readLine()) != null){
+            while((line = leitor.readLine()) != null){
                 String[] campos = line.split(";");
                 String cpf = campos[0];
                 String nome = campos[1];
                 cpfnome.put(cpf, nome);
             }
-            reader.close();
+            leitor.close();
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo " + e.getMessage());
         }
