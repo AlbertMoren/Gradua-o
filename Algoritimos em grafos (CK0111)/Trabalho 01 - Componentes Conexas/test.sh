829747d4e578ac508d628cf5b43aa6f7b48332de
AlbertMoren <albertmorenn@gmail.com>
2024-07-24 22:35:37 -0300

    fix: add bfs algorithnm

--- Algoritimos em grafos (CK0111)/Trabalho 01 - Componentes Conexas/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {0..118}
-do
-    cat instancias/$i.in | ./programa.py > minhaSaida/saida_$i.txt
-    diff -s minhaSaida/saida_$i.txt solucoes/$i.out
-done
-
---
+++ Algoritimos em grafos (CK0111)/busca_em_largura.py
@@ -0,0 +1,40 @@
+import grafo
+
+g = grafo.criar_grafo()
+g = grafo.add_vertex(g,0)
+g = grafo.add_vertex(g,2)
+g = grafo.add_vertex(g,1)
+g = grafo.add_vertex(g,3)
+g = grafo.add_vertex(g,4)
+grafo.add_edge(g,0,1)
+grafo.add_edge(g,2,1)
+grafo.add_edge(g,0,1)
+grafo.add_edge(g,3,1)
+grafo.add_edge(g,4,2)
+grafo.print_graf(g)
+
+from queue  import Queue
+def bfs(grafo, V):
+    no_visited = []
+    visited = []
+    for i in range(len(grafo)):
+        no_visited.append(i)
+    fila = Queue()
+    fila.put(V)
+    no_visited.remove(V)
+    while not fila.empty():
+        U = fila.get()
+        visited.append(U)
+        for i in grafo[U]:
+            if i in no_visited:
+                fila.put(i)
+                no_visited.remove(i)
+    return visited
+
+ordem = bfs(g,0)
+
+for i in range(len(ordem)):
+    if i < len(ordem) - 1:
+        print(f"{ordem[i]} -> ",end="")
+    else:
+        print(f"{ordem[i]}")
